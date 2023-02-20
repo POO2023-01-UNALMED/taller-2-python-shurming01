982aece280da6b9652aec8964bd1fe8445b8d678
shurming01 <salashiguita.rs@gmail.com>
2023-02-20 12:22:22 -0500

    commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-class auto:
+class Auto:
+
     cantidadCreados = 0
     def __init__(self, modelo, precio, asientos, marca, motor, registro):
           self.modelo = modelo
@@ -7,6 +8,60 @@ class auto:
           self.marca = marca
           self.motor = motor
           self.registro = registro
+
+    def cantidadAsientos(self):
+        contador = 0
+        for Asiento in self.asientos:
+            if isinstance(self.asientos, Asiento):
+                contador += 1
+        return contador
+
+    def verficarIntegridad(self):
+
+        for Asiento in self.asientos:
+            if isinstance(self.asientos, Asiento):
+                if self.asientos.regsistro != self.registro:
+                    return "Las piezas no son originales"
+        if self.motor.registro != self.registro:
+            return "Las piezas no son originales"
+        return "Auto original"
+
+class Asiento:
+    def __anit__ (self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+    def cambiarColor(self, color):
+        permitidos = ['rojo', 'verde', 'amarillo', 'negro', 'blanco']
+
+        if color in permitidos:
+            self.color = color
+
+class Motor:
+    def __init__ (self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+        
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+
+    def asignarTipo(self, tipo):
+        tipoMotor = ['electrico', 'gasolina']
+
+        if tipo in tipoMotor:
+            self.tipo = tipo
+
+
+
+
+
+
+
+
+
+
           
              
 
