04f004c09b67339be19fbd77de48ee6e9103b490
shurming01 <salashiguita.rs@gmail.com>
2023-02-20 15:45:20 -0500

    commit

--- main.py
+++ main.py
@@ -11,13 +11,12 @@ class Auto:
 
     def cantidadAsientos(self):
         contadorasientos = 0
-        for Asiento in self.asientos:
+        for self.asientos in Asiento:
             if isinstance (self.asientos, Asiento):
                 contadorasientos += 1
         return contadorasientos
 
     def verficarIntegridad(self):
-
         for  Asiento in self.asientos:
             if isinstance(self.asientos, Asiento):
                 if self.asientos.regsistro != self.registro:
