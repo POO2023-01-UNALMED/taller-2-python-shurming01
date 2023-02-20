da625b3088481a5f30e7ea228c702c080d3c8c55
shurming01 <salashiguita.rs@gmail.com>
2023-02-20 00:54:07 -0500

    commit

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-class asiento:
-    def__init__(self, modelo, precio, asientos, marca, motor, registro, CantidadCreados):
-    
-    self.modelo = 
+class auto:
+    cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+          self.modelo = modelo
+          self.precio = precio
+          self.asientos = asientos
+          self.marca = marca
+          self.motor = motor
+          self.registro = registro
+          
              
 
 
